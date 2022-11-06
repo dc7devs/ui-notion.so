0040e45cd4271918182f61c3e96c5d5cd9738c19
Diego C. Silva <diegocaetano444@outlook.com>
2022-11-06 04:18:22 -0300

    PW: implementing to the script, functionality to change the visibility of the primary-butto of the navigation bar

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,16 +3,19 @@
 const space = document.querySelector("div.js-space");
 let header = document.querySelector("header");
 let header_menubar = document.querySelector("header div.header-menubar");
+let primary_button = document.querySelector("li.nav-right-tnf-item");
 let didScroll=false;
 
 function styleMenuBar() {    
     header.setAttribute("style", "height: 60px; transition: .3s");
     header_menubar.setAttribute("style", "border-bottom: 1px solid var(--border-color-regular)");
+    primary_button.classList.add("inscroll");
 }
 
 function styleOldMenuBar() {
     header.setAttribute('style', 'height: 80px; transition: .5s');
     header_menubar.setAttribute('style', "border-bottom: 0");
+    primary_button.classList.remove("inscroll");
 }
 
 window.addEventListener('scroll', () => {
